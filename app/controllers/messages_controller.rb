683442dd60bf0534baf18adc0b0de8e80f039160
Yuki Nakata <dorcushopeino1@gmail.com>
2019-07-07 08:16:01 +0000

    display messages using partial (real time message create)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,7 @@ class MessagesController < ApplicationController
 		message = current_user.messages.build(message_params)
 		if message.save
 			ActionCable.server.broadcast "chatroom_channel",
-																		foo: message.body
+																		mod_message: message_render(message)
 		end
 	end
 	
@@ -16,4 +16,10 @@ class MessagesController < ApplicationController
 	def message_params
 		params.require(:message).permit(:body)
 	end
+	
+	def message_render(message)
+		render(partial: 'message', locals: {message: message})
+	end
+	
+	
 end
